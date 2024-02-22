9fc63794d39b2361b8edc1a24c0aaa2c7e357b92
A13x37 <145167791+A13x37@users.noreply.github.com>
2024-02-21 17:24:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Shows popup for placing an order
 const popupBox = document.querySelector("#popupBox");
+
 function openPopup() {
     const popup = document.createElement('div');
     popup.classList.add('popup');
@@ -33,3 +34,86 @@ function closeSideBar() {
 
 
 // Adds order to list
+let list = document.querySelector("#list");
+
+let alfredoPrice = 0;
+let alfredoQuantity = 0;
+
+let pizzaPrice = 0;
+let pizzaQuantity = 0;
+
+let spagettyPrice = 0;
+let spagettyQuantity = 0;
+
+let cakePrice = 0;
+let cakeQuantity = 0;
+
+
+function addOrder(image, name) {
+    let item = document.createElement('div');
+
+    if (name == 'Chicken Alfredo') {
+        alfredoPrice += 6.98;
+        alfredoQuantity++;
+        item.innerHTML = `<div style="display: flex; justify-content: space-evenly; margin-top: 15px;">
+                            <img src="./images/${image}" style="height: 100px; width: 100px; object-fit: cover;">
+                            <div>
+                            <h3>${name}</h3>
+                            <p style="margin-top: 10px;">Price: $${alfredoPrice}</p>
+                            <p style="margin-top: 10px;">Quantity: ${alfredoQuantity}
+                            </div></div>`;
+    } else if (name == 'Pizza') {
+        pizzaPrice += 8.98;
+        pizzaQuantity++;
+        item.innerHTML = `<div style="display: flex; justify-content: space-evenly; margin-top: 15px;">
+                        <img src="./images/${image}" style="height: 100px; width: 100px; object-fit: cover;">
+                        <div>
+                        <h3>${name}</h3>
+                        <p style="margin-top: 10px;">Price: $${pizzaPrice}</p>
+                        <p style="margin-top: 10px;">Quantity: ${pizzaQuantity}
+                        </div></div>`;
+    } else if (name == 'Spagetty') {
+        spagettyPrice += 6.98;
+        spagettyQuantity++;
+        item.innerHTML = `<div style="display: flex; justify-content: space-evenly; margin-top: 15px;">
+                        <img src="./images/${image}" style="height: 100px; width: 100px; object-fit: cover;">
+                        <div>
+                        <h3>${name}</h3>
+                        <p style="margin-top: 10px;">Price: $${spagettyPrice}</p>
+                        <p style="margin-top: 10px;">Quantity: ${spagettyQuantity}
+                        </div></div>`;
+    } else if (name == 'Lemon Cheesecake') {
+        cakePrice += 4.59;
+        cakeQuantity++;
+        item.innerHTML = `<div style="display: flex; justify-content: space-evenly; margin-top: 15px;">
+                        <img src="./images/${image}" style="height: 100px; width: 100px; object-fit: cover;">
+                        <div>
+                        <h3>${name}</h3>
+                        <p style="margin-top: 10px;">Price: $${cakePrice}</p>
+                        <p style="margin-top: 10px;">Quantity: ${cakeQuantity}
+                        </div></div>`;
+    }
+
+    removeItem();
+    list.appendChild(item);
+}
+
+function removeItem() {
+    if (alfredoQuantity > 1) {
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+          }
+    } else if (pizzaQuantity > 1) {
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+          }
+    } else if (spagettyQuantity > 1) {
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+          }
+    } else if (cakeQuantity > 1) {
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+          }
+    }
+}
